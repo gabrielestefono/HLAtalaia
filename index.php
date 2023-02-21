b70d83e74b70f92f189ae589cd39e0e8518a1655
gabrielestefono <gabrielestefono@hotmail.com>
2023-02-21 15:10:58 -0300

    Barra de Navegação

--- index.php
+++ index.php
@@ -1,3 +1,41 @@
 <?php
-
-echo "Funcionando!";
+include_once("helpers/url.php");
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HL Atalaia - Início</title>
+    <link rel="stylesheet" href="<?=$BASE_URL?>/assets/css/style.css">
+</head>
+<body>
+    <section class="header">
+        <div class="header__top">
+            <div class="header__top--left">
+                <a href="<?=$BASE_URL?>"><img class="header__top--left_image" src="<?=$BASE_URL?>/assets/img/logo.png"></a>
+            </div>
+            <div class="header__top--right">
+                <div class="cart-icon">
+                    <a href="<?=$BASE_URL?>">
+                    <div  class="cart-icon__icon"></div>
+                    <div class="cart-icon__items">0</div>
+                    </a>
+                </div>
+                <div class="profile-icon">
+                    <a href="<?=$BASE_URL?>">
+                    <div class="profile-icon__image"></div>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="header__bottom">
+            <div class="header__bottom--bars">
+                <div class="header__bottom--bar"></div>
+            </div>
+            <div class="header__bottom--searchbar">
+                <input type="text" class="header__bottom--searchbar_area" placeholder="Pesquisa"></input><div class="header__bottom--searchbar_space"><div class="header__bottom--searchbar_space-icon"></div></div>
+            </div>
+        </div>
+    </section>
